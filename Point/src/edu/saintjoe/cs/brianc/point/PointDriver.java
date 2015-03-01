1976b89bb5ded2b5acdcbeb3c8e0524d733e34a8
Brian Capouch <brianc@saintjoe.edu>
2015-03-01 16:38:04 -0500

    Add driver class

---
+++ Point/src/edu/saintjoe/cs/brianc/point/PointDriver.java
@@ -0,0 +1,29 @@
+package edu.saintjoe.cs.brianc.point;
+
+public class PointDriver {
+
+	public static void main(String[] args ) {
+	
+	// Create three points
+	Point lowerLeft = new Point();
+	Point upperRight = new Point();
+	Point middlePoint = new Point();
+
+	// Give them some values
+	lowerLeft.x = 0.0;
+	lowerLeft.y = 0.0;
+
+	upperRight.x = 1280.0;
+	upperRight.y = 1024.0;
+
+	middlePoint.x = 640.0;
+	middlePoint.y = 512.0;
+
+	// Print one of the points
+	upperRight.printPoint();
+	upperRight.setPoint(15.0, 15.0);
+	upperRight.printPoint();
+	middlePoint.printPoint();
+	System.out.println("The distance between your points is " + upperRight.distance(middlePoint));
+	}
+}
